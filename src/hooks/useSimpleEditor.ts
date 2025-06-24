c3db308657960583e7223745521d5acee1c67760
hm <squirrel309@naver.com>
2025-06-24 17:11:07 +0900

    resizeImage 익스텐션 inline true 설정

--- src/hooks/useSimpleEditor.ts
+++ src/hooks/useSimpleEditor.ts
@@ -150,7 +150,9 @@ export function useSimpleEditor({
       TaskList,
       TaskItem.configure({ nested: true }),
       Highlight.configure({ multicolor: true }),
-      ImageResize,
+      ImageResize.configure({
+        inline: true,
+      }),
       Typography,
       Superscript,
       Subscript,
--- vite.config.ts
+++ vite.config.ts
@@ -10,4 +10,19 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  build: {
+    minify: "terser",
+    terserOptions: {
+      compress: {
+        drop_console: true, // 콘솔 로그를 제거
+        drop_debugger: true, // 디버거 구문을 제거
+      },
+      output: {
+        comments: false, // 주석제거
+      },
+    },
+  },
+  server: {
+    port: 3100,
+  },
 });
